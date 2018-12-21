a7217670157275cf6efce356f980cb083911ec59
Zach Ferland <zachferland@gmail.com>
2018-12-20 20:32:06 -0500

    feat: enable cors

--- src/index.js
+++ src/index.js
@@ -15,4 +15,9 @@ const server = new ApolloServer({
   })
 });
 
-exports.graphqlHandler = server.createHandler();
+exports.graphqlHandler = server.createHandler({
+  cors: {
+    origin: '*',
+    credentials: true,
+  },
+})
